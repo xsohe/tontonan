b855919a55cd5718c0f0c6031dbc0f04b522b5e7
YanuarEka <yanuarekasusila@gmail.com>
2025-04-02 10:55:15 +0700

    feat: slicing login and sign up

---
+++ resources/js/Components/Button.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+PropTypes.Button = {
+    type: PropTypes.oneOf(["submit", "button", "reset"]),
+    className: PropTypes.string,
+    variant: PropTypes.oneOf([
+        "primary",
+        "warning",
+        "danger",
+        "light-outline",
+        "white-outline",
+    ]),
+    processing: PropTypes.bool,
+    children: PropTypes.node,
+};
+
+export default function Button({
+    type = "submit",
+    variant = "primary",
+    processing,
+    className = "",
+    children,
+    ...props
+}) {
+    return (
+        <button
+            {...props}
+            type={type}
+            className={`rounded-2xl py-[13px] text-center w-full btn-${variant} ${
+                processing && "opacity-30"
+            }`}
+            disabled={processing}
+        >
+            {children}
+        </button>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,15 @@ use Inertia\Inertia;
 |
 */
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+Route::redirect("/", "/prototype/login");
+
+Route::prefix("prototype")->name("prototype.")->group(function () {
+    Route::get("/login", function () {
+        return Inertia::render("Prototype/Login");
+    })->name("login");
+    Route::get("/register", function () {
+        return Inertia::render("Prototype/Register");
+    })->name("register");
 });
 
 Route::get('/dashboard', function () {
