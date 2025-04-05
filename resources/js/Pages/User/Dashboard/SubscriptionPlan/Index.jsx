34d63bb8abc3cffb02535edd1e3c6f7d7f8b248c
YanuarEka <yanuarekasusila@gmail.com>
2025-04-05 12:09:08 +0700

    feat: integrate midtrans

--- resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
+++ resources/js/Pages/User/Dashboard/SubscriptionPlan/Index.jsx
@@ -1,19 +1,48 @@
 import Authenticated from "@/Layouts/Authenticated/Authenticated";
 import SubscriptionCard from "@/Components/SubscriptionCard";
-import { Head, router } from "@inertiajs/react";
+import { router, Head } from "@inertiajs/react";
 
-export default function SubscriptionPlan({ auth, subscriptionPlans }) {
+export default function SubscriptionPlan({ auth, subscriptionPlans, env }) {
     const selectSubscription = (id) => {
         router.post(
             route("user.dashboard.subscription-plan.userSubscribe", {
                 subscriptionPlan: id,
-            })
+            }),
+            {},
+            {
+                only: ["userSubscription"],
+                onSuccess: ({ props }) => {
+                    onSnapMidtrans(props.userSubscription);
+                },
+            }
         );
+    };
+
+    const onSnapMidtrans = (userSubscription) => {
+        snap.pay(userSubscription.snap_token, {
+            // Optional
+            onSuccess: function (result) {
+                router.visit(route("user.dashboard.index"));
+            },
+            // Optional
+            onPending: function (result) {
+                console.log(result);
+            },
+            // Optional
+            onError: function (result) {
+                console.log(result);
+            },
+        });
     };
 
     return (
         <>
-            <Head title="Subscription Plan" />
+            <Head title="Subscription Plan">
+                <script
+                    src="https://app.sandbox.midtrans.com/snap/snap.js"
+                    data-client-key={env.MIDTRANS_CLIENTKEY}
+                />
+            </Head>
             <Authenticated auth={auth}>
                 <div className="py-20 flex flex-col items-center">
                     <div className="text-black font-semibold text-[26px] mb-3">
